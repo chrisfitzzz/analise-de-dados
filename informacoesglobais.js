d87126061fc539fb0db58699edaafbbcdfa41021
chrisfitzzz <113901551+chrisfitzzz@users.noreply.github.com>
2024-10-22 12:20:46 -0300

    Update informacoesglobais.js

--- informacoesglobais.js
+++ informacoesglobais.js
@@ -3,18 +3,15 @@ const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-g
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
-    const carrosVendidos = (dados.total_carros_vendidos / 1e9)
-    const celtasNoBrasil = (dados.total_celtas_brasil / 1e9)
-    const horas = parseInt(dados.tempo_usado)
-    const minutos = Math.round((dados.tempo_usado - horas) * 100)
-    const porcentagemUsada = ((carrosVendidos / celtasNoBrasil ) * 100).toFixed(2)
+    const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas) * 100)
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
 
-    console.log(dados);
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    
-    paragrafo.innerHTML = `voce sabia que existem aproximadamente <span>${dados.total_celtas_brasil}1.667.202</span> unidades de celtas no brasil, foram vendidos <span>${dados.total_carros_vendidos}1.057.366 </span>unidades de celtas no brasil. Por dia, os celtas sao usados por <span>${dados.tempo_usado}</span>2.38 horas por pessoa.`
-    console.log(paragrafo)
+    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
