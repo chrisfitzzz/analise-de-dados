39bfaf94ef9c4aa31acbfad94b48898e32b43e59
chrisfitzzz <113901551+chrisfitzzz@users.noreply.github.com>
2024-10-22 12:08:09 -0300

    Update informacoesglobais.js

--- informacoesglobais.js
+++ informacoesglobais.js
@@ -1,14 +1,23 @@
-const url='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json';
-//url='20.201.28.151/main/dados/grafico-principal.json';
-//modelo de endereço servidor de dados para o javaScript.
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
-    const res= await fetch(url)
-    const dados = await res.json
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    const carrosVendidos = (dados.total_carros_vendidos / 1e9)
+    const celtasNoBrasil = (dados.total_celtas_brasil / 1e9)
+    const horas = parseInt(dados.tempo_usado)
+    const minutos = Math.round((dados.tempo_usado - horas) * 100)
+    const porcentagemUsada = ((carrosVendidos / celtasNoBrasil ) * 100).toFixed(2)
+
     console.log(dados);
-    const paragrafo = document.createElement('p');
-    paragrafo.classList.add('graficos-container__texto');
-    paragrafo.innerHTML = 'Você sabia que o mundo tem <span>${dados.total_de_pessoas_mundo}</span> de pessoas e que aproximadamente <span>${dados.total_pessoas_conectadas}</span> estão em alguma rede social e passam em média <span>${dados.tempo_medio}</span>horas conectadas.'
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    
+    paragrafo.innerHTML = `voce sabia que existem aproximadamente <span>${dados.total_celtas_brasil}1.667.202</span> unidades de celtas no brasil, foram vendidos <span>${dados.total_carros_vendidos}1.057.366 </span>unidades de celtas no brasil. Por dia, os celtas sao usados por <span>${dados.tempo_usado}</span>2.38 horas por pessoa.`
+    console.log(paragrafo)
+
+    const container = document.getElementById('graficos-container')
+    container.appendChild(paragrafo)
 }
 
-visualizarInformacoesGlobais();
+vizualizarInformacoesGlobais()
